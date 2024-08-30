2917774f6e7318fc90819dc6350cfd5eb863adc0
web223dev <ruslanus866@outlook.com>
2024-08-30 18:33:56 +0800

    Change title, Add globalStyles, Button, Heading, Input, Row styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
+
+const StyledApp = styled.main`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Wild Oasis</Heading>
+
+            <div>
+              <Heading as='h2'>Check in and out</Heading>
+              <Button onClick={() => alert('Check in')}>Check in</Button>
+              <Button variation='secondary' size='medium' onClick={() => alert('Check out')}>
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as='h3'>Form</Heading>
+            <form>
+              <Input type='number' placeholder='Number of guests' />
+              <Input type='number' placeholder='Number of guests' />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;

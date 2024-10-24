5900b2c0b8d83d810162a5be58f5c85a3364058b
web223dev <ruslanus866@outlook.com>
2024-10-24 17:02:52 +0800

    Filtering Booking - APISide

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,16 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export default async function getBookings() {
-  const { data, error } = await supabase
+export default async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
+
+  if (filter !== null) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);

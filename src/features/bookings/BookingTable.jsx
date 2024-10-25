d94e2b3cc90654d24a2a6e79ea1731f86db2a3d3
web223dev <ruslanus866@outlook.com>
2024-10-25 22:24:47 +0800

    Paginating Bookings - APISide Pagination

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -7,7 +7,7 @@ import Spinner from "../../ui/Spinner";
 import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { isLoading, bookings } = useBookings();
+  const { isLoading, bookings, count } = useBookings();
 
   if (isLoading) return <Spinner />;
 
@@ -30,7 +30,7 @@ function BookingTable() {
           render={(booking) => <BookingRow key={booking.id} booking={booking} />}
         />
         <Table.Footer>
-          <Pagination count={46} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>

341c2ab2556ee4d7cab840d9926054f6f2eff566
web223dev <ruslanus866@outlook.com>
2024-11-18 23:07:23 +0800

    update app email and password

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,7 +7,7 @@ import useLogin from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("flowerman866@gmail.com");
+  const [email, setEmail] = useState("ross@example.com");
   const [password, setPassword] = useState("1234");
   const { login, isLoading } = useLogin();
 
